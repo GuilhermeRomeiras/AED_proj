4261173ca6cecc85acc9cfea56e8cf9feba6d5dc
GuilhermeRomeiras <150958361+GuilhermeRomeiras@users.noreply.github.com>
2025-10-20 16:30:23 +0100

    Add files via upload

--- file_results.c
+++ file_results.c
@@ -2,12 +2,27 @@
 #define FILE_results_c
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "header.h"
 
-void print_results(FILE *ptr_results_file, Sol* Solucao){
-for (int i = 0; i < Solucao->caminho_size; i++)
-{
-fprintf(" %i ", Solucao->caminho[i]);
+void print_results(FILE *ptr_results_file, Sol *Solucao, int *automovel, Cli *cliente){
+if(Solucao->valida == 1){
+        fprintf(ptr_results_file, "%i ", cliente->id);
+        for (int i = 0; i < Solucao->caminho_size; i++){
+            fprintf(ptr_results_file, "%i %s ", Solucao->caminho[i]+1, Enum_int_to_str(automovel[Solucao->caminho_id[i]]));
+        }
+        fprintf(ptr_results_file,"%i ", cliente->cidade_destino + 1);
+        fprintf(ptr_results_file, "%i %i\n", Solucao->tempo_total, Solucao->custo_total);
+    }
+    else
+    {
+        fprintf(ptr_results_file, "%i -1", cliente->id);
+    }
+    
+    
+free(Solucao->caminho_id);
+free(Solucao->caminho);
+
 }
-}
+
 #endif
